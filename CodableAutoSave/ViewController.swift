685d06709d6038badd8e70955b6969104f159544
LINDSAY, BRETT D [AG/1000] <brett.d.lindsay@monsanto.com>
2017-09-20 17:36:34 -0500

    Initial Commit

---
+++ CodableAutoSave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodableAutoSave
+//
+//  Created by LINDSAY, BRETT D [AG/1000] on 9/20/17.
+//  Copyright © 2017 Brett Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
